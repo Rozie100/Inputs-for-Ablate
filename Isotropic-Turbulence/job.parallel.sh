b677b9955d9c5ad31736976545d57af5b64127d1
Rozie100 <52797564+Rozie100@users.noreply.github.com>
2022-10-30 13:02:16 -0400

    Add files via upload

---
+++ Isotropic-Turbulence/job.parallel.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#SBATCH --cluster=ub-hpc
+#SBATCH --partition=general-compute --qos=general-compute
+#SBATCH --time=72:00:00
+#SBATCH --nodes=80
+#SBATCH --ntasks-per-node=40
+#SBATCH --constraint=NIH
+#SBATCH --cpus-per-task=1
+#SBATCH --job-name="turbBox"
+
+
+# Print the current environment
+echo "SLURM_JOBID="$SLURM_JOBID
+echo "SLURM_JOB_NODELIST"=$SLURM_JOB_NODELIST
+echo "SLURM_NNODES"=$SLURM_NNODES
+echo "SLURMTMPDIR="$SLURMTMPDIR
+
+echo "working directory = "$SLURM_SUBMIT_DIR
+
+
+module use /projects/academic/chrest/modules
+module load chrest/release
+
+# The initial srun will trigger the SLURM prologue on the compute nodes.
+NPROCS=`srun --nodes=${SLURM_NNODES} bash -c 'hostname' |wc -l`
+echo NPROCS=$NPROCS
+
+# The PMI library is necessary for srun
+export I_MPI_PMI_LIBRARY=/usr/lib64/libpmi.so
+
+
+export HDF5_ROOT=$PETSC_DIR/$PETSC_ARCH
+# Make a temp directory so that tchem has a place to vomit its files
+
+
+
+# Run ABLATE with Input
+echo "Start Time " `date +%s`
+srun -n $NPROCS  ~/release/ablate --input /panasas/scratch/grp-chenjm/rozie/turbBox.yaml
+
+
+echo "End Time " `date +%s`
